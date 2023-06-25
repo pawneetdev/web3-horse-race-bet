ad4ed9447934af342e454aed90adf64c254d5e5d
Robin R <robin.r@coindcx.com>
2023-06-25 13:45:13 +0530

    user sol change

--- abstracts/i_user_data.sol
+++ abstracts/i_user_data.sol
@@ -6,14 +6,29 @@ abstract contract IUserStorage {
     mapping(uint256 => User) usersData;
     uint256 usersCount;
 
-    function createUser(string memory name, address walletAddress) public {
+    function createUser(string memory name, address walletAddress)
+        public
+        returns (User memory)
+    {
         uint256 newUserId = usersCount + 1;
         User memory newUser = User(newUserId, walletAddress, name);
         usersData[newUserId] = newUser;
         usersCount = usersCount + 1;
+        return newUser;
     }
 
-    function logInUser(uint256 userId) public view returns (User memory) {
-        return usersData[userId];
+    function logInUser()
+        public
+        view
+        returns (User memory)
+    {
+        User memory loginUserModel;
+        for (uint256 i = 0; i < usersCount; i++) {
+            if (usersData[i + 1].userWalletAddress == msg.sender) {
+                loginUserModel = usersData[i + 1];
+                break ;
+            }
+        }
+        return loginUserModel;
     }
 }

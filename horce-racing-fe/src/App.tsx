c476a91ba4da69af875c571d2e15aa23a6eafc12
Robin R <robin.r@coindcx.com>
2023-06-26 11:34:48 +0530

    route protection

--- horce-racing-fe/src/App.tsx
+++ horce-racing-fe/src/App.tsx
@@ -1,19 +1,22 @@
 import './App.scss';
 import BiddingComponent from "./components/BidComponent/BidComponent"
 import HomeComponent from "./components/Home/HomeComponent"
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import ProtfolioComponent from "./components/PortfolioComponent/ProtfolioComponent";
 import SigninSignup from "./components/SigninSignup/SigninSignup";
+import WalletContext from './store/WalletContext';
+import { useContext } from 'react';
 
 function App() {
+  const { user } = useContext(WalletContext);
   return (
     <div className="App">
       <Layout />
       <Routes>
         <Route path="/" element={<HomeComponent/>}>
         </Route>
-        <Route path="/bid" element={<BiddingComponent/>}>
+        <Route path="/bid" element={ user.Id !== 0 ? <BiddingComponent/> : <Navigate to="/login" /> }>
         </Route>
         <Route path="/portfolio" element={<ProtfolioComponent/>}>
         </Route>

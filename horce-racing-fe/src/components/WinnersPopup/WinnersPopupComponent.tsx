47e32a079db74b5c20ae3038068e94b2f26e2bb9
Robin R <robin.r@coindcx.com>
2023-06-27 17:10:08 +0530

    getting winners

--- horce-racing-fe/src/components/WinnersPopup/WinnersPopupComponent.tsx
+++ horce-racing-fe/src/components/WinnersPopup/WinnersPopupComponent.tsx
@@ -13,28 +13,62 @@ interface LoadingPopupIntf {
 
 interface WinnerIntf {
   betType: Bets;
-  winners: string[];
+  winners: {name: string, amount: number}[];
 }
+
+const mapBetType = (type: number) => {
+  switch (type) {
+    case 0:
+      return Bets.Straight;
+    case 1:
+      return Bets.Place;
+    case 2:
+      return Bets.Show;
+  }
+  return Bets.Straight;
+}
+
 
 const WinnersPopupComponent: React.FC<LoadingPopupIntf> = ({loadPopup, raceId, closePopup}) => {
   const [isLoading, setIsLoading] = useState(false);
   const [winners, setWinners] = useState<WinnerIntf[]>([]);
-  const { contract } = useContext(WalletContext)
-  const openPopup = async (raceId: number) => {
-    // const winners = await contract.getRaceBets(raceId);
-    const winners: WinnerIntf[] = [
-      {
-        betType: Bets.Show,
-        winners: ['Robin', 'Samson']
-      },
-      {
-        betType: Bets.Place,
-        winners: ['Robin']
+  const { contract } = useContext(WalletContext);
+
+  const getUserName = async(userId: number) => {
+    const name = await contract.getUsername(userId);
+    return name;
+  }
+
+  const createWinnerMap = async(races: any, raceId: number) => {
+    const winners: WinnerIntf[] = [];
+    let winner = {} as any;
+    for(let i = 0; i < races.length; i++) {
+      if(parseInt(races[i].raceId) === raceId) {
+        const betType = mapBetType(races[i].betType);
+        const winningPrize = (parseInt(races[i].winningPrize) / 10**18);
+        if (winningPrize > 0) {
+          const name = await getUserName(parseInt(races[i].userId));
+          if(winner[betType]) {
+            winner[betType].push({ name: name, amount: winningPrize });
+          } else {
+            winner[betType] = [{ name: name, amount: winningPrize }];
+          }
+        }
       }
-    ]
-    setWinners(winners);
+    }
+    for(const key in winner) {
+      winners.push({ betType: key as Bets, winners: winner[key] });
+    }
+    return winners;
+  }
+
+  const openPopup = async (raceId: number) => {
+    const winners = await contract.getRaceBets(raceId);
+    const mapped = await createWinnerMap(winners, raceId);
+    setWinners(mapped);
     setIsLoading(true);
   };
+  
   const close = () => {
     setIsLoading(false);
     closePopup();
@@ -55,7 +89,12 @@ const WinnersPopupComponent: React.FC<LoadingPopupIntf> = ({loadPopup, raceId, c
             return (
               <div key={winner.betType}>
                 <Typography variant="subtitle1">Bet type: {winner.betType}</Typography>
-                <Typography variant="subtitle1">Winners: {winner.winners.join(', ')}</Typography>
+                <Typography variant="subtitle1">Winners:</Typography>
+                {winner.winners.map((w) => {
+                  return(
+                    <Typography key={w.name} variant="subtitle1">Name: {w.name}, Amount: {w.amount}</Typography>
+                  );
+                })}
               </div>
             )
           })}
